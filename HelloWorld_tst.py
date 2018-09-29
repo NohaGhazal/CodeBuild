8056bf2f7c2adf16b6721d2ee6c930547ee28ed3
Ghazal <nohaghaz@dca90495b2e8.ant.amazon.com>
2018-09-29 20:37:17 +0100

    yakraimyarab

--- HelloWorld_tst.py
+++ HelloWorld_tst.py
@@ -4,7 +4,8 @@ from HelloWorld import HelloWorld
 class MyTestCase(unittest.TestCase):
   def test_default_greeting_set(self):
     hw = HelloWorld()
+    self.assertEqual(hw.message, 'Hello world!')
     self.assertEqual(hw.message, 'Hello Noha!')
-
 if __name__ == '__main__':
+  unittest.main()
   unittest.main()
